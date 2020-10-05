457aed31fd60bdcab4ab741cd29c7eaabb7fb6d5
otonielalonso <otonielalonso@localhost>
2020-10-04 18:06:34 -0700

    Initial commit of project Alonso, Otoniel CSE360

---
+++ Dummy/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,57 @@
+
+
+
+
+
+package cse360assignment02;
+
+/**
+ * This class implements an adding machine,
+ * @author otonielalonso
+ * @version 1.0
+ */
+
+
+public class AddingMachine {
+	//class body
+  private int total;
+  
+  /**
+   * 
+   * @param total the total value to be manipulated and returned in functions
+   */
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  /**
+   * This returns this total value
+   * @return this current total value
+   */
+  public int getTotal () {
+    return 0;
+  }
+  
+  /**
+   * This will add the int value passed through it
+   * @return added value
+   */
+  public void add (int value) {
+  }
+  /**
+   * This will subtract the int value passed through it
+   * @return subtracted value
+   */
+  public void subtract (int value) {
+  }
+  /**
+   * This will create a toString
+   * @return toString created
+   */
+  public String toString () {
+    return "";
+  }
+
+  public void clear() {
+  }
+}
