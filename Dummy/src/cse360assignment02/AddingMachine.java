ba1602a72e5bc224c26fb69d796d859b0bf9ee91
otonielalonso <otonielalonso@localhost>
2020-10-04 20:11:15 -0700

    Created new StringBuffer, completed methods, added new comments.

--- Dummy/src/cse360assignment02/AddingMachine.java
+++ Dummy/src/cse360assignment02/AddingMachine.java
@@ -1,3 +1,8 @@
+/**
+ * This class implements an adding machine,
+ * @author otonielalonso
+ * @version 1.1
+ */
 
 
 
@@ -5,16 +10,15 @@
 
 package cse360assignment02;
 
-/**
- * This class implements an adding machine,
- * @author otonielalonso
- * @version 1.0
- */
 
 
 public class AddingMachine {
-	//class body
+	/**
+	 * Create a private total to keep track of total
+	 * StringBuffer list will be used for our toString
+	 */
   private int total;
+  StringBuffer list; 
   
   /**
    * 
@@ -23,35 +27,59 @@ public class AddingMachine {
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    list = new StringBuffer("0");
   }
   /**
    * This returns this total value
    * @return this current total value
    */
   public int getTotal () {
-    return 0;
+	  
+    return total;
   }
   
   /**
    * This will add the int value passed through it
-   * @return added value
+   * @param total plus this value
+   * We then add this value to our history list
    */
   public void add (int value) {
+	  
+	  total = total + value; 
+	  list.append(" + " + value);
+	   
+	  
+	  
   }
   /**
    * This will subtract the int value passed through it
-   * @return subtracted value
+   * @param total subtracted by value
+   * We then add this value to our list
    */
   public void subtract (int value) {
+	  
+	  total = total - value;
+	  list.append(" - " + value);
   }
   /**
    * This will create a toString
    * @return toString created
    */
   public String toString () {
-    return "";
+    return list.toString();
   }
 
+  /**
+   * This will delete the 'history' of our list
+   * It will begin at 1 since we want to always initialize the history at 0
+   * We then reset the total value back to 0 to start over
+   */
   public void clear() {
+	  
+	  list.delete(1, list.length());
+	  total = 0; 
+	  
+	  
+	  
   }
 }
